8b7cc64c295427854fc93c0bfe56acd6c993951f
little <vitaly.little@gmail.com>
2022-10-17 02:38:40 +0400

    List item layout, add ImageView.

--- app/src/main/java/com/app/pizza/ItemsAdapter.kt
+++ app/src/main/java/com/app/pizza/ItemsAdapter.kt
@@ -3,15 +3,16 @@ package com.app.pizza
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.app.pizza.model.Item
 
 class ItemsAdapter(private val items: List<Item>) : RecyclerView.Adapter<ItemsAdapter.ItemViewHolder>(){
     inner class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        var tittleTextView         : TextView = view.findViewById(R.id.title)
-        var priceTextView          : TextView = view.findViewById(R.id.price)
-        var descriptionTextView    : TextView = view.findViewById(R.id.description)
+        var tittleTextView         : TextView  = view.findViewById(R.id.title)
+        var priceTextView          : TextView  = view.findViewById(R.id.price)
+        var descriptionTextView    : TextView  = view.findViewById(R.id.description)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
