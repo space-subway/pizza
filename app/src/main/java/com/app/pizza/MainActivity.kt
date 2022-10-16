d78d50ccc64b6b1e54e417535f6bbcf5e4bc7d66
little <vitaly.little@gmail.com>
2022-10-17 00:36:30 +0400

    Move action bar from activity to menu fragment. Toolbar background color.

--- app/src/main/java/com/app/pizza/MainActivity.kt
+++ app/src/main/java/com/app/pizza/MainActivity.kt
@@ -2,14 +2,10 @@ package com.app.pizza
 
 import android.os.Bundle
 import android.view.Menu
-import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.viewpager.widget.ViewPager
 import com.app.pizza.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -20,40 +16,11 @@ class MainActivity : AppCompatActivity() {
     // onDestroyView.
     private val binding get() = _binding!!
 
-    lateinit var viewPager: ViewPager
-    lateinit var viewPagerAdapter: SlidePagerAdapter
-    lateinit var headerSliderList: List<Int>
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        setSupportActionBar(binding.toolbarMainActivity)
-        //binding.toolbarMainActivity.title = ""
-
-        viewPager = binding.sliderViewPager
-
-        // on below line we are initializing
-        // our image list and adding data to it.
-        headerSliderList = ArrayList<Int>()
-        headerSliderList = headerSliderList + R.drawable.header_slide_1
-        headerSliderList = headerSliderList + R.drawable.header_slide_2
-        headerSliderList = headerSliderList + R.drawable.header_slide_3
-
-        // on below line we are initializing our view
-        // pager adapter and adding image list to it.
-        viewPagerAdapter = SlidePagerAdapter(this@MainActivity, headerSliderList)
-
-        // on below line we are setting
-        // adapter to our view pager.
-        viewPager.adapter = viewPagerAdapter
-
-        //fill toolbar dropdown exposed menu
-        val dropdownItems = listOf("Option 1", "Option 2", "Option 3", "Option 4")
-        val adapter = ArrayAdapter(this, R.layout.dropdown_menu_list_item, dropdownItems)
-        (binding.menuTextField as? AutoCompleteTextView)?.setAdapter(adapter)
 
         val navView: BottomNavigationView = binding.bottomNavigation
 
@@ -65,7 +32,6 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_menu, R.id.navigation_profile, R.id.navigation_basket
             )
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 
--- app/src/main/java/com/app/pizza/MenuFragment.kt
+++ app/src/main/java/com/app/pizza/MenuFragment.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import androidx.fragment.app.Fragment
 import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
 import com.app.pizza.databinding.FragmentMenuBinding
 import com.app.pizza.model.Item
 
@@ -15,6 +18,10 @@ class MenuFragment: Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    lateinit var viewPager: ViewPager
+    lateinit var viewPagerAdapter: SlidePagerAdapter
+    lateinit var headerSliderList: List<Int>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,5 +44,30 @@ class MenuFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        //setSupportActionBar(binding.toolbar)
+        //binding.toolbarMainActivity.title = ""
+
+        viewPager = binding.sliderViewpager
+
+        // on below line we are initializing
+        // our image list and adding data to it.
+        headerSliderList = ArrayList<Int>()
+        headerSliderList = headerSliderList + R.drawable.header_slide_1
+        headerSliderList = headerSliderList + R.drawable.header_slide_2
+        headerSliderList = headerSliderList + R.drawable.header_slide_3
+
+        // on below line we are initializing our view
+        // pager adapter and adding image list to it.
+        viewPagerAdapter = SlidePagerAdapter(requireContext(), headerSliderList)
+
+        // on below line we are setting
+        // adapter to our view pager.
+        viewPager.adapter = viewPagerAdapter
+
+        //fill toolbar dropdown exposed menu
+        val dropdownItems = listOf("Option 1", "Option 2", "Option 3", "Option 4")
+        val adapter = ArrayAdapter(requireContext(), R.layout.dropdown_menu_list_item, dropdownItems)
+        (binding.menuTextField as? AutoCompleteTextView)?.setAdapter(adapter)
     }
 }
